7544f11a3df6a8ce94696273a3fbec90d262b9be
SmerchProsto <smerchprosto@gmail.com>
2019-07-15 23:18:48 +0400

    Update animate.js

--- code-examples/animate.js
+++ code-examples/animate.js
@@ -18,3 +18,13 @@ function animate(options) {
 
   });
 }
+// example of using the function
+// animate({
+//   duration: 1000,
+//   timing: function(timeFraction) {
+//     return timeFraction;
+//   },
+//   draw: function(progress) {
+//     elem.style.width = progress * 100 + '%';
+//   }
+// });
